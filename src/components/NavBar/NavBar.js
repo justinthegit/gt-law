8ac2e009fb6630656a010f973c4152d4d17973cf
Justin Low <jlow2567@gmail.com>
2019-03-05 16:14:45 +1030

    typgoraphy plugin not workgin

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { } from './style.js'
+
+class Teaser extends Component {
+  render() {
+    return (
+      <div>
+        <h2>there's more</h2>
+        <h1>shouldnt this text be in brandon grotesque?? and auto capitalized</h1>
+      </div>
+    );
+  }
+}
+
+export default Teaser;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { NavBar } from "../components/NavBar";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <NavBar/>
     <p>Now go build something great.</p>
     <h2>hi</h2>
     <p>helllaldsoaoodsao</p>
