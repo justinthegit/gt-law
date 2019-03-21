fc00911264e36b8c5a9183de2fef719a6d752763
Jon Low <jon@avenue.studio>
2019-03-21 16:31:03 +1100

    preload font files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,34 @@
  */
 
 // You can delete this file if you're not using it
+const React = require('react')
+const fs = require('fs')
+
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const files = getFilesFromPath('./public/static', '.woff2')
+
+  const aPreload = files.map((file, key) => {
+    return (
+      <link
+        key={key}
+        rel="preload"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+        href={`/static/${file}`}
+      />
+    )
+  })
+
+  const existingHeadComponents = getHeadComponents();
+  const mergedHeadComponents = aPreload.concat(existingHeadComponents);
+
+  replaceHeadComponents(
+    mergedHeadComponents
+  )
+}
+
+function getFilesFromPath(path, extension) {
+  let dir = fs.readdirSync(path)
+  return dir.filter(elm => elm.match(new RegExp(`.*\.(${extension})`, 'ig')))
+}
