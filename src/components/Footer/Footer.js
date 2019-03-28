0dfadb6487a50f7c7073bb88a92fb42d8816f9f0
Jon Low <jon@avenue.studio>
2019-03-28 17:47:01 +1100

    use mailto in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -32,7 +32,7 @@ class Footer extends Component {
 					</a>
 
 					<p>
-					Gilbert + Tobin is an equal opportunity employer. If you require assistance accessing any of the information contained here, please contact Kristie Barton on <a className="tel" href="tel:0292634575">(02) 9263 4575</a> or via <a href="/"> gtcareers@gtlaw.com.au  </a>
+					Gilbert + Tobin is an equal opportunity employer. If you require assistance accessing any of the information contained here, please contact Kristie Barton on <a className="tel" href="tel:0292634575">(02) 9263 4575</a> or via <a href="mailto:gtcareers@gtlaw.com.au"> gtcareers@gtlaw.com.au  </a>
 					</p>
 				</Wrapper>
 
